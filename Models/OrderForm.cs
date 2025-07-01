6aac2b10269afd3afea9e91c304d1cb914047eaf
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2025-07-01 18:45:18 -0500

    edit to models

--- Models/OrderForm.cs
+++ Models/OrderForm.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace clumsyapparel.Models;
 
 public class OrderForm
@@ -5,11 +7,13 @@ public class OrderForm
     public int Id { get; set; }
     public required string VinylType { get; set; }
     public required string ShirtType { get; set; }
-    public decimal Price { get; set; }
+    public required decimal Price { get; set; }
     public decimal HoursLogged { get; set; }
     public int AmountOfErrors { get; set; }
     public bool NeededHelp { get; set; }
+     [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
     public int Rating { get; set; }
+    [MaxLength(500, ErrorMessage = "Notes cannot exceed 500 characters.")]
     public string? Notes { get; set; }
     public string? ImageURL { get; set; }
     public bool Fulfilled { get; set; }
