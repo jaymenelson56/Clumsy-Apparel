380ad68643be36163e1b0d385ca11c01425490e7
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2025-10-09 20:27:42 -0500

    added get orderform by id, made updated on nullable

--- Migrations/20251010005621_MakeUpdatedOnNullable.Designer.cs
+++ Migrations/20251010005621_MakeUpdatedOnNullable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using clumsyapparel.Data;
@@ -11,9 +12,11 @@ using clumsyapparel.Data;
 namespace ClumsyApparel.Migrations
 {
     [DbContext(typeof(clumsyapparelDbContext))]
-    partial class clumsyapparelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251010005621_MakeUpdatedOnNullable")]
+    partial class MakeUpdatedOnNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
